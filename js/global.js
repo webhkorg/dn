1dfb2d2e5f039efdb02634713a10790f2d1c110f
shing19861006 <webhkorg@gmail.com>
2025-02-06 06:01:32 +0800

    202502060601

--- js/global.js
+++ js/global.js
@@ -6,3 +6,17 @@ window.addEventListener('scroll', function () {
         header.classList.remove('fixed');
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    if (document.getElementById('menu') && document.getElementById('menuOpenButton')) {
+        const menu = document.getElementById('menu');
+        const menuOpenButton = document.getElementById('menuOpenButton');
+        const menuCloseButton = document.getElementById('menuCloseButton');
+        menuOpenButton.addEventListener('click', () => {
+            menu.classList.toggle('open');
+        });
+        menuCloseButton.addEventListener('click', () => {
+            menu.classList.remove('open');
+        });
+    }
+});
